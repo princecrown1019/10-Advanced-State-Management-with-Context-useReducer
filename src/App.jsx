ae18f68d83987023178bc97a44205ac21f96b909
princecrown1019 <princecrown1019@gmail.com>
2025-01-30 20:19:14 +0200

    shopping cart context definition

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import { CartContext } from './store/shopping-context-context.jsx';
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
